ea3fc650a58680079f209ad0c5216f5e850da62c
Anushree G M <117091147+AnushreeGM@users.noreply.github.com>
2025-06-28 12:01:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "ami_id" {
   description = "The AMI ID to use for the instance"
   type        = string
+  default     = "ami-00c8ac9147e19828e"
 }
 
 variable "instance_type" {
   description = "The type of the instance"
   type        = string
+  default     = "t3.micro"
 }
